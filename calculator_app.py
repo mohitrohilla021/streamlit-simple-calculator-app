e78d718d9ce5a9322bfd66a46a176bae98059e9d
MOHIT ROHILLA <49117131+mohitrohilla021@users.noreply.github.com>
2023-12-23 22:12:32 +0530

    Update calculator_app.py

--- calculator_app.py
+++ calculator_app.py
@@ -1,25 +1,27 @@
 # Importing packages
 import streamlit as st
 
+# Header display in the App
 st.header('Basic Calculator APP')
 
+# Operation selection
 list_of_operation = ['Addition', 'Subtraction', 'Multiplication', 'Division']
-
 operation = st.selectbox('Choose an operation', options=list_of_operation)
 
-input_1 = st.number_input('Input 1')
-input_2 = st.number_input('Input 2')
+# Number inputs
+input_1 = st.sidebar.number_input('Input 1')
+input_2 = st.sidebar.number_input('Input 2')
 
-# Performing operation
+# Performing operation based upon the input
 if operation == 'Addition':
-    st.write('Result:', input_1+input_2)
+    st.write('Result of '+str(input_1)+' '+operation+' with '+str(input_2)+' is:', input_1+input_2)
 elif operation == 'Subtraction':
-    st.write('Result:', input_1-input_2)
+    st.write('Result of '+str(input_1)+' '+operation+' with '+str(input_2)+' is:', input_1-input_2)
 elif operation == 'Multiplication':
-    st.write('Result:', input_1*input_2)
+    st.write('Result of '+str(input_1)+' '+operation+' with '+str(input_2)+' is:', input_1*input_2)
 elif operation == 'Division':
     if input_2==0:
         raise ValueError('Attempt to division by Zero, not allowed in maths!!!')
-    st.write('Result:', input_1/input_2)
+    st.write('Result of '+str(input_1)+' '+operation+' with '+str(input_2)+' is:', input_1/input_2)
 
 st.write('Thanks for using the service')
